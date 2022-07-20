aff0360b444fd91a1b6a3e8b9fcef9fd039f4b3e
Bilobra <bra.carrozzo@gmail.com>
2022-07-20 15:02:40 +0200

    add const km + age

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+// chiedere il numero di chilometri che vuole percorrere
+
+// chiedere l’età del passeggero 
+
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+
+// NUMERO CHILOMETRI
+
+// dichiarazione variabile
+
+const chilometri = prompt('Quanti chilometri vuole percorrere? Inserire solo il numero')
+
+// stamp variabile
+
+console.log('numero di km : ' + chilometri)
+
+
+// ETA' PASSEGGERO
+
+// dichiarazione variabile
+
+const etaPasseggero = prompt('Quanti anni ha ? Inserire solo il numero')
+
+// stamp variabile
+
+console.log('età del passeggero : ' + etaPasseggero)
