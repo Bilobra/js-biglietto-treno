a05d74310b1da952bea9466b1da333262d55a09a
Bilobra <bra.carrozzo@gmail.com>
2022-07-20 15:55:18 +0200

    add variable scount final

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 // va applicato uno sconto del 40% per gli over 65.
 
 // Se l'età del passeggero è > 65
-    // il presso avrà sconto del 40%
+// il presso avrà sconto del 40%
 
 
 
@@ -19,7 +19,7 @@
 
 // dichiarazione variabile
 
-const chilometri = parseInt(prompt('Quanti chilometri vuole percorrere? Inserire solo il numero')) 
+const chilometri = parseInt(prompt('Quanti chilometri vuole percorrere? Inserire solo il numero'))
 
 // stamp variabile
 
@@ -64,21 +64,46 @@ console.log('prezzo totale senza sconto : ' + totalPrice + ' \u20AC')
 // ---------------------------
 // SCONTO 20% <18
 
-const ventiPerc = (totalPrice * ( 1 - 0.2 )).toFixed(2)
+const ventiPerc = (totalPrice * (1 - 0.2)).toFixed(2)
 console.log('prezzo scontato 20% : ' + ventiPerc)
 
 // SCONTO 40% >65
 
-const quarantaPerc = (totalPrice * ( 1 - 0.4 ) ).toFixed(2)
+const quarantaPerc = (totalPrice * (1 - 0.4)).toFixed(2)
 console.log('prezzo scontato 40% : ' + quarantaPerc)
 
 
 // .toFixed alla fine del calcolo, tiene in considerazione solo un certo numero
 // di cifre decimali, (2) ---> ne considera solo due
 
+
+
+
+
 // -----------------------------
+// dichiarare la variable con id = password con DOCUMENT + GET e prendendo la voce che rappresenta l'Id
+const trainTicketDomElement = document.getElementById('train_price')
+// controllare quale valore attribuire all' Id = password con CONSOLE.DIR
+console.dir(trainTicketDomElement)
+// stampiamo del contenuto nel nostro h1 con id password ,
+//  trovando il valore che rappresenta il nostro testo, nell'elenco generato da console.dir
+// nel nostro caso è INNER.HTML
 
-// variabile per > 65
+// -------------------------------------
 
+
+// variabile per calcolare gli sconti legati a km ed età
+if (etaPasseggero > 65) {
+    trainTicketDomElement.innerHTML = (quarantaPerc)
+    console.dir(trainTicketDomElement.innerHTML)
+
+} else if (etaPasseggero < 18) {
+    trainTicketDomElement.innerHTML = (ventiPerc)
+    console.dir(trainTicketDomElement.innerHTML)
+}
+else {
+    trainTicketDomElement.innerHTML = (totalPrice)
+    console.dir(trainTicketDomElement.innerHTML)
+}
 
 
